cb2f0ccbd662da1c82c2c583809e39b74212922f
Corey <camonsma@hotmail.com>
2023-06-21 16:57:39 -0400

    version 2

--- static/js/app.js
+++ static/js/app.js
@@ -18,7 +18,7 @@ const fullDataSet = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-cl
  function buildCharts(sample)
  {
   d3.json(fullDataSet).then(function(data) {
-  
+    
     //Use sample_values as the values for the bar chart.
     let sample_values = data.samples;
     let temp = sample_values.filter(obj=>obj.id==sample)
@@ -60,7 +60,7 @@ Use sample_values for the marker size.
 Use otu_ids for the marker colors.
 Use otu_labels for the text values.
 */
-
+alert(sample);
 //Use sample_values as the values for the bar chart.
      let sample_values = data.samples;
      let temp = sample_values.filter(obj=>obj.id==sample)
@@ -94,28 +94,79 @@ Use otu_labels for the text values.
     });
    };
 
-   /////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
    function buildGageCharts(sample)
    {
     d3.json(fullDataSet).then(function(data) {
  //Use sample_values as the values for the bar chart.
  let sample_values = data.metadata;
- console.log(data);
+// console.log(data);
  let temp = sample_values.filter(obj=>obj.id==sample)
- console.log(temp[0]);
- //let myresults = temp[0];
- //let graph_sample_values = myresults.sample_values;
- //let otu_ids = myresults.otu_ids;
- //let otu_labels = myresults.otu_labels;
+// console.log(temp[0]);
+ let metadata = temp[0];
+ let id = metadata.id;
+ let eth = metadata.ethnicity;
+ let sex = metadata.gender;
+ let age = metadata.age;
+ let loc = metadata.location;
+ let bbt = metadata.bbtype;
+ let fre = metadata.wfreq;
 
- 
+let panelBody = d3.select(".panel-body");
+panelBody.append("h3").text(`id: ${id}`);
+panelBody.append("h3").text(`ethnicity: ${eth}`);
+panelBody.append("h3").text(`gender: ${sex}`);
+panelBody.append("h3").text(`age: ${age}`);
+panelBody.append("h3").text(`location: ${loc}`);
+panelBody.append("h3").text(`bbtype: ${bbt}`);
+panelBody.append("h3").text(`wfreq: ${fre}`);
      });
     };
+///////////////////////////////////////////////////////////////////////////////////////////////
+function buildOptions()
+{
+ d3.json(fullDataSet).then(function(data) {
+//Get the list of name/numbers from the JSON file pull
+let name_values = data.names;
+//get the html selection option we want to add our options in.
+let selectD = d3.select("#selDataset");
 
- /////////////////////////////////////////////////////////////////////////////////////////////
+// Assign the value of the dropdown menu option to a letiable
+//let dataset = dropdownMenu.property("value");
+// Initialize an empty array for the country's data
+
+for(i=0;i<name_values.length;i++)
+ {
+ //let val = selectD.property("value"); 
+  selectD.append("option").text(name_values[i]);
+//  selectD.setAttribute("value",name_values[i]);
+ };
+  });
+ };
+///////////////////////////////////////////////////////////////////////////////////////////////
+//optionChanged(this.value)
+//similar to class exercise 3 final demo
+// On change to the DOM, call getData()
+function optionChanged(value)
+{
+ //alert('corey');
+ //alert(value);
+ let iValue = parseInt(value);
+ console.log(iValue);
+  buildCharts(iValue);
+  buildPopCharts(iValue);
+ //buildGageCharts(iValue);
+ //buildOptions();
+
+};
+/////////////////////////////////////////////////////////////////////////////////////////////
+// only to confirm data was read completely
+
+/////////////////////////////////////////////////////////////////////////////////////////////
  buildPopCharts(940);
  buildCharts(940);
  buildGageCharts(940);
- 
+ buildOptions();
+
 //Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
 
